426fe214353a70b09223c6813cc3d2662cc330b5
sinazaeiftan <31307099+sinazaeiftan@users.noreply.github.com>
2017-08-29 11:19:23 +0200

    Lektioner2 min Car change

--- src/lektion1/Bil.java
+++ src/lektion1/Bil.java
@@ -5,9 +5,11 @@ public class Bil {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       int ANTALAET RATTAR = 4;
+       int ANTALAET_RATTAR = 4;
        
-       System.out.printIn(ANTALET _RATTAR)
+       System.out.println(ANTALAET_RATTAR);
+	}
+}
        
        
 	
