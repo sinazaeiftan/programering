fbfcd063dfcb67ad31beb3df672e9937a57783d6
sinazaeiftan <31307099+sinazaeiftan@users.noreply.github.com>
2017-08-29 11:15:31 +0200

    LEKTION 1 Bil

---
+++ src/lektion1/Bil.java
@@ -0,0 +1,15 @@
+package lektion1;
+
+public class Bil {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int ANTALAET RATTAR = 4;
+       
+       System.out.printIn(ANTALET _RATTAR)
+       
+       
+	
+
+
