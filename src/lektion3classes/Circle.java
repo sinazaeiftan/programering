2af19e44608c93f097b3f8901cea9078c64e307b
sinazaeiftan <31307099+sinazaeiftan@users.noreply.github.com>
2017-09-05 11:22:59 +0200

    summary
    lektion 3

---
+++ src/lektion3classes/Circle.java
@@ -0,0 +1,8 @@
+package lektion3classes;
+
+public class Circle {
+    public double radien = 1.0;
+    public String color = "red";
+    
+    
+}
---
+++ src/lektion3classes/lektion 3.java
@@ -0,0 +1,12 @@
+package lektion3classes;
+
+public class Testcircle {
+
+	public static void main(String[] args) {
+     Circle jonas = new Circle ();
+     
+     System.out.println(jonas.color);
+ 
+	}
+
+}sss
