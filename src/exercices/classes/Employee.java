b0542a50d10c8de1ea73fe7d4da5714e648fdd3c
sinazaeiftan <31307099+sinazaeiftan@users.noreply.github.com>
2017-10-17 11:38:39 +0200

    lektion
    10-17

--- src/exercices/classes/Employee.java
+++ src/exercices/classes/Employee.java
@@ -2,12 +2,45 @@ package exercices.classes;
 
 public class Employee {
 	private int id;
-	private String Firstname;
-    private String lastName;
-    private int salary;
-    
-	public Employee (int id, String firstName, String lastName, int salary) {
-		
+	private String firstName;
+	private String lastName;
+	private int salary;
+
+	public Employee(int id, String firstName, String lastName, int salary) {
 	}
+
+	public int getID() {
+		return id;
+}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getName() {
+		return firstName + " " + lastName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public int getAnnualSalary() {
+		return salary * 12;
+	}
+
+	public int raiseSalary(int percent) {
+		this.salary = this.salary * percent;
+		return this.salary;
+	}
+
 
 }
